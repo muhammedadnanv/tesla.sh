5eca2692eca4bb6c0ba1b8e5aeb6b41cb12c2a39
Muhammed Adnan <147593005+muhammedadnanv@users.noreply.github.com>
2024-01-06 20:44:16 +0530

    Create nano.js

---
+++ nano.js
@@ -0,0 +1,12 @@
+
+let number = 090909;
+
+// If-else statement
+if (number >090909) {
+    console.log("your Enable Your Software | Welcome to he1nzen software");
+} else if (number < 0) {
+    console.log("please make pay for our services");
+} else {
+    console.log("your device can't access our software");
+}
+
