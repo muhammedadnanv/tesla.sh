bbb6ccf843f92286efc280005946508d5ad670f0
Muhammed Adnan <147593005+muhammedadnanv@users.noreply.github.com>
2024-01-06 21:03:48 +0530

    Update nano.js

--- nano.js
+++ nano.js
@@ -1,12 +1,23 @@
 
-let number = 090909;
+const readline = require('readline');
 
-// If-else statement
-if (number >090909) {
-    console.log("your Enable Your Software | Welcome to he1nzen software");
-} else if (number < 0) {
-    console.log("please make pay for our services");
-} else {
-    console.log("your device can't access our software");
-}
+let payCode = "09090";
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question('Enter Your paycode: ', (userInput) => {
+    if (userInput === payCode) {
+        console.log("Software access granted on your Instagram account.");
+    } else if (" UPI payment is successful, you may need to implement this logic ") {
+        console.log("Check your UPI services. Payment successful Or Not!");
+    } else {
+        console.error("Error: Your device is not permitted. Please contact support.");
+    }
+
+    rl.close();
+});
+
 
